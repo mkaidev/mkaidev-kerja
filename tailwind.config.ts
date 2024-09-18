54dd52ce0d7ddcccbd11ce4a78310ed643466a8d
mkaidev <mkaidev88@gmail.com>
2024-09-18 18:09:44 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config = {
 	darkMode: ['class'],
-	content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+	content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/features/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{ts,tsx}'],
 	prefix: '',
 	theme: {
 		container: {
